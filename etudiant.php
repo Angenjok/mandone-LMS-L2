d9d2c10f2062a1c0fdb4c10cc7fe50b57fe5caf6
Angenjok <ebolokomarie@gmail.com>
2026-06-13 15:05:23 +0100

    Create etudiant.php

---
+++ etudiant.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+include 'db.php';
+
+// Vérification de sécurité
+if ($_SESSION['role'] !== 'etudiant') {
+    die("Accès réservé aux étudiants.");
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Espace Étudiant</title>
+</head>
+<body>
+    <h2>Liste des cours disponibles</h2>
+    <ul>
+        <?php
+        $result = $conn->query("SELECT * FROM lecons");
+        while ($row = $result->fetch_assoc()) {
+            echo "<li>" . $row['titre'] . " - <a href='" . $row['fichier_url'] . "' target='_blank'>Accéder au cours</a></li>";
+        }
+        ?>
+    </ul>
+</body>
+</html>
